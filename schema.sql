55de7f3fc11832635488e344f637b0f88a4c1356
sgaio <sgaio81@gmail.com>
2019-09-18 03:59:33 -0700

    first commit

---
+++ schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+    item_id INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(45) NOT NULL,
+    department_name VARCHAR(45) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT(10) NOT NULL,
+    primary key(item_id)
+);
+
+SELECT * FROM products;
---
+++ seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO products ( product_name,  department_name, price, stock_quantity)
+VALUES ("Spiders", "bugs", 45.00, 10),
+("lady bugs", "bugs", 30.00, 10), 
+("cockroaches", "bugs", 15.00, 1), 
+("ants", "pets", 10.00, 5);
